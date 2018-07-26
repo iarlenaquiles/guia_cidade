8a2081d258ea82b1855403ef9e41227377d45de6
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-26 09:31:27 -0300

    Criando o formgroup do login

--- src/pages/auth/login/login.ts
+++ src/pages/auth/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -8,12 +9,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
+  public loginForm: FormGroup;
+
   backgrounds = [
     'assets/imgs/background/background-1.jpeg',
     'assets/imgs/background/background-2.jpg'
   ];
   
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public formBuilder: FormBuilder) {
+      this.loginForm = this.formBuilder.group({
+        email: ['', Validators.required],
+        senha: ['', Validators.compose([Validators.minLength(6), Validators.maxLength(20), Validators.required])]
+      });
   }
 
   ionViewDidLoad() {
