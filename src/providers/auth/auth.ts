99b5576093c4ac4cf72e7fa7f37198077244e071
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-27 11:07:19 -0300

    Criando o método para salvar o novo usuario

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,17 +1,44 @@
+import { AngularFireDatabase } from 'angularfire2/database';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
-/*
-  Generated class for the AuthProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class AuthProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AuthProvider Provider');
+  constructor(public angularFireAuth: AngularFireAuth,
+    public angularFireDatabase: AngularFireDatabase,
+    public http: HttpClient) {
+
+  }
+
+  signupUser(usuario): any {
+    return this.angularFireAuth.auth.createUserWithEmailAndPassword(usuario.email, usuario.senha).then((newUser) => {
+      return this.angularFireAuth.auth.signInWithEmailAndPassword(usuario.email, usuario.senha).then((authenticatedUser) => {
+        let uid = authenticatedUser.user.uid;
+        let userObject = {
+          uid: uid,
+          registeredDate: Date.now(),
+          name: usuario.nome + ' ' + usuario.sobrenome,
+          email: usuario.email,
+          photoUrl: ''
+        };
+
+        newUser.user.updateProfile({
+          displayName: usuario.nome + " " + usuario.sobrenome,
+          photoURL: ""
+        });
+
+        return this.angularFireDatabase.list('userProfile').update(uid, userObject).then(() => true,
+          error => {
+            throw new Error(error.message);
+          });
+      }, error => {
+        throw new Error(error.message);
+      });
+    }, error => {
+      throw new Error(error.message);
+    });
   }
 
 }
