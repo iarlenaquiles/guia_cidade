6942e54e85def459868d124087031dba27bb9008
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-27 18:01:25 -0300

    Configurando o authprovider

--- src/pages/auth/login/login.ts
+++ src/pages/auth/login/login.ts
@@ -1,5 +1,6 @@
+import { AuthProvider } from './../../../providers/auth/auth';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @IonicPage()
@@ -15,14 +16,16 @@ export class LoginPage {
     'assets/imgs/background/background-1.jpeg',
     'assets/imgs/background/background-2.jpg'
   ];
-  
-  constructor(public navCtrl: NavController, 
-    public navParams: NavParams, 
-    public formBuilder: FormBuilder) {
-      this.loginForm = this.formBuilder.group({
-        email: ['', Validators.required],
-        senha: ['', Validators.compose([Validators.minLength(6), Validators.maxLength(20), Validators.required])]
-      });
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder,
+    public authProvider: AuthProvider,
+    public loading: LoadingController) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      senha: ['', Validators.compose([Validators.minLength(6), Validators.maxLength(20), Validators.required])]
+    });
   }
 
   ionViewDidLoad() {
@@ -31,5 +34,21 @@ export class LoginPage {
 
   goToSignup() {
     this.navCtrl.push('SignupPage');
+  }
+
+  facebookLogin() {
+    let loading = this.loading.create({
+      content: 'Realizando login com Facebook'
+    });
+
+    loading.present();
+
+    this.authProvider.facebookLogin().then(() => {
+      loading.dismiss();
+      alert('Login Realizado');
+    }, error => {
+      loading.dismiss();
+      alert(JSON.stringify(error));
+    });
   }
 }
