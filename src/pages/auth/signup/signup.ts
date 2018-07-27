33423b98af2df6de158a68310925c0870d001828
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-27 16:33:03 -0300

    Criando o método de signup

--- src/pages/auth/signup/signup.ts
+++ src/pages/auth/signup/signup.ts
@@ -1,6 +1,7 @@
+import { AuthProvider } from './../../../providers/auth/auth';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,21 +16,46 @@ export class SignupPage {
     'assets/imgs/background/background-1.jpeg',
     'assets/imgs/background/background-2.jpg'
   ];
-  
-  constructor(public navCtrl: NavController, 
-    public navParams: NavParams, 
-    public formBuilder: FormBuilder) {
-      this.signupForm = this.formBuilder.group({
-        email: ['', Validators.required],
-        senha: ['', Validators.compose([Validators.minLength(6), Validators.maxLength(20), Validators.required])],
-        senhaConfirmacao: ['', Validators.compose([Validators.minLength(6), Validators.maxLength(20), Validators.required])],
-        nome: ['', Validators.required],
-        sobrenome: ['', Validators.required]
-      });
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder,
+    public alert: AlertController,
+    public authProvider: AuthProvider,
+    public loading: LoadingController) {
+    this.signupForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      senha: ['', Validators.compose([Validators.minLength(6), Validators.maxLength(20), Validators.required])],
+      senhaConfirmacao: ['', Validators.compose([Validators.minLength(6), Validators.maxLength(20), Validators.required])],
+      nome: ['', Validators.required],
+      sobrenome: ['', Validators.required]
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
+  }
+
+  signup() {
+    let { senha, senhaConfirmacao } = this.signupForm.controls;
+    let senhaErrada = senha.value !== senhaConfirmacao.value;
+
+    if (senhaErrada) {
+      const alert = this.alert.create({
+        title: 'Ops...',
+        subTitle: 'Senhas nao conferem',
+        buttons: ['OK']
+      });
+
+      alert.present();
+    } else {
+      let loading = this.loading.create({
+        content: 'Criando usuário...'
+      });
+      loading.present();
+      
+      this.authProvider.signupUser(this.signupForm.value);
+    }
   }
 
 }
